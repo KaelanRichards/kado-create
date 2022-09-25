8043c073f84eb3ff90fe837dbae4f8135fa2e1c1
Kaelan Richards <kadokaelan@gmail.com>
2022-09-24 22:58:34 -0700

    fixed build errors

--- libs/markdown/src/lib/markdown.ts
+++ libs/markdown/src/lib/markdown.ts
@@ -1,14 +1,13 @@
 import { readFileSync } from 'fs';
 import matter from 'gray-matter';
 import { join } from 'path';
-import { MarkdownDocument } from './types';
 
 import { serialize } from 'next-mdx-remote/serialize';
 
 export const getParsedFileContentBySlug = (
   fileName: string,
   postPath: string
-): MarkdownDocument => {
+) => {
   const fullPath = join(postPath, `${fileName}.mdx`);
   const fileContent = readFileSync(fullPath);
 
